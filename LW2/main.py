236b948a29857ba81365a157882a03dd6aa5c15b
VitaliyKolbasiuk <vitaliykolbasiuk05@gmail.com>
2024-12-13 00:33:00 +0200

    Use StandardScaler

--- LW2/main.py
+++ LW2/main.py
@@ -3,6 +3,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.linear_model import LinearRegression, Ridge, Lasso, ElasticNet
 from sklearn.metrics import mean_squared_error, mean_absolute_error, mean_absolute_percentage_error
+from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 
 ALPHAS = np.logspace(-4, 4, 10)
@@ -37,6 +38,10 @@ X = df[['n_non_stop_unique_tokens', 'timedelta', 'n_tokens_title',
         'average_token_length', 'LDA_03', 'num_imgs', 'num_videos']]
 y = df['shares']
 
+# Підрахунок початкової кількості спостережень
+initial_count = X.shape[0]
+print(f"Початкова кількість спостережень: {initial_count}")
+
 # Виявлення та видалення викидів
 outlier_indices = np.unique(np.concatenate([
     detect_outliers(df['shares']),
@@ -46,8 +51,17 @@ outlier_indices = np.unique(np.concatenate([
 X_cleaned = X.drop(index=outlier_indices).reset_index(drop=True)
 y_cleaned = y.drop(index=outlier_indices).reset_index(drop=True)
 
+# Підрахунок кількості спостережень після видалення викидів
+final_count = X_cleaned.shape[0]
+print(f"Кількість спостережень після видалення викидів: {final_count}")
+print(f"Видалено {initial_count - final_count} спостережень.")
+
 # Розділення набору даних
 X_train, X_test, y_train, y_test = train_test_split(X_cleaned, y_cleaned, test_size=0.2, random_state=RANDOM_STATE)
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
 
 # Лінійна регресія
 linear_model = LinearRegression()
