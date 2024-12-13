70cedc38283ad57e8dc4862c6d0491951136c91a
VitaliyKolbasiuk <vitaliykolbasiuk05@gmail.com>
2024-12-13 08:18:08 +0200

    Update

--- FinalProject/preprocessing.py
+++ FinalProject/preprocessing.py
@@ -7,9 +7,9 @@ from sklearn.pipeline import Pipeline
 def load_and_preprocess_data(data_path: str):
     df = pd.read_csv(data_path, delimiter=',')
     print(f"Кількість змінних до препроцессінгу: {df.shape[1]}")
-    # Видаляємо стовпці з більш ніж 50% пропусків
+    # Видаляємо стовпці з більш ніж 75% пропусків
     important_columns = ['target', 'important_feature']
-    df = df.loc[:, (df.isnull().sum() <= len(df) * 0.55) | df.columns.isin(important_columns)]
+    df = df.loc[:, (df.isnull().sum() <= len(df) * 0.75) | df.columns.isin(important_columns)]
     # Видаляємо стовпчики, які містять id, voice, calls, sms, count_url_category_
     df = df.drop(df.filter(regex='^voice|id|^sms|^count_url_category_|calls').columns, axis=1)
     print(f"Кількість змінних після препроцессінгу: {df.shape[1]}")
