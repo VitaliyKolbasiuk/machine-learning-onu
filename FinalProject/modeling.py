715b2b8723fc953864b739b0f93760c41750fa30
VitaliyKolbasiuk <vitaliykolbasiuk05@gmail.com>
2024-12-13 08:42:04 +0200

    Some fixes

--- FinalProject/modeling.py
+++ FinalProject/modeling.py
@@ -1,4 +1,4 @@
-from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score, roc_auc_score
+from sklearn.metrics import precision_score, recall_score, f1_score, roc_auc_score
 
 
 def evaluate_model(model, X_test, y_test, model_name="Model"):
@@ -6,6 +6,5 @@ def evaluate_model(model, X_test, y_test, model_name="Model"):
     print(f"{model_name} - Precision: {precision_score(y_test, y_pred, zero_division=1)}")
     print(f"{model_name} - Recall: {recall_score(y_test, y_pred, zero_division=1)}")
     print(f"{model_name} - F1: {f1_score(y_test, y_pred)}")
-    print(f"{model_name} - Accuracy: {accuracy_score(y_test, y_pred)}")
     print(f"{model_name} - ROC AUC: {roc_auc_score(y_test, y_pred)}")
     return y_pred
